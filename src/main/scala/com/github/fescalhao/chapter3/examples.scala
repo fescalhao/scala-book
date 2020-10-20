d02c282067b95e211aa5295bf7ec32995a161ba7
Felipe Alberto Silva Escalhao <fescalhao@uolinc.com>
2020-10-20 18:22:45 -0300

    Arrays e Listas

--- src/main/scala/com/github/fescalhao/chapter3/examples.scala
+++ src/main/scala/com/github/fescalhao/chapter3/examples.scala
@@ -1,9 +1,9 @@
 package com.github.fescalhao.chapter3
 
 object examples extends App {
-  param_example()
+  add_elem_to_beginning()
 
-//  --------------------------------- Parameterize arrays with types ---------------------------------
+// --------------------------------- Array ---------------------------------
   // Example: param_example()
   def param_example() = {
     val greetings = new Array[String](3)
@@ -16,5 +16,41 @@ object examples extends App {
     for(i <- 0 to 2) {
       println(greetings(i))
     }
+  }
+
+  // Example: create_array()
+  def create_array() = {
+    val num = Array("zero", "one", "two")
+    num.foreach(x => println(x))
+
+    val numVerbose = Array.apply("three", "four", "five")
+    numVerbose.foreach(x => println(x))
+  }
+// ------------------------------------------------------------------------
+
+// --------------------------------- List ---------------------------------
+  // Example: create_list()
+  def create_list() = {
+    val list = List(1,2,3)
+    list.foreach(x => println(x))
+  }
+
+  def concat_list() = {
+    val list1 = List(1,2,3)
+    val list2 = List(4,5,6)
+    val list3 = list1 ::: list2
+
+    // list1 and list2 were not mutated. list3 is a new List
+    println(list1)
+    println(list2)
+    println(list3)
+  }
+
+  def add_elem_to_beginning() = {
+    val list = List(2,3)
+    val newList = 1 :: list
+    val emptyList = List(Nil)
+
+    println(newList)
   }
 }
