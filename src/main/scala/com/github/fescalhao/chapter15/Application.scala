857a5d49378cda8a83ac2f2df41b22b17ae164db
Felipe <escalhao3@gmail.com>
2020-11-27 15:57:08 -0300

    Case classes and pattern matching finished

--- src/main/scala/com/github/fescalhao/chapter15/Application.scala
+++ src/main/scala/com/github/fescalhao/chapter15/Application.scala
@@ -2,199 +2,31 @@
 
 package com.github.fescalhao.chapter15
 
-import com.github.fescalhao.chapter15.classes.{BinOp, Expr, Number, UnOp, Var}
+import com.github.fescalhao.chapter15.classes.{BinOp, Examples, Expr, ExprFormatter, Number, UnOp, Var}
 
 object Application extends App {
 
-  val expr = UnOp("+", UnOp("abs", Var("x")))
+  val example = Examples()
+//  example.executeExamples()
 
-  println(simplifyTop(expr))
-  println("-------------------")
-  checkOpKind(expr)
-  println("-------------------")
-  println(describe(5))
-  println("-------------------")
-  println(isPi)
-  println("-------------------")
-  println(isList(expr))
-  println("-------------------")
-  println(generalSize(List(1, 2, 3)))
-  println("-------------------")
-  checkArgumentType()
-  println("-------------------")
-  println(describe2(Number(2)))
-  println("-------------------")
-  capitals()
-  println("-------------------")
-  variablePatter()
-  println("-------------------")
-  caseSequences()
-  println("-------------------")
-  forPattern()
+  val f = new ExprFormatter
 
-  def simplifyTop(expr: Expr): Expr = {
-    expr match {
-      case UnOp("-", UnOp("-", e)) => e
-      case BinOp("+", e, Number(0)) => e
-      case BinOp("+", e, Number(1)) => e
-      case _ => expr
-    }
-  }
+  val e1 = BinOp(
+    "*",
+    BinOp("+", Var("x"), Number(1)),
+    BinOp("/", Number(1), Number(2)),
+  )
 
-  def simplifyAll(expr: Expr): Expr = {
-    expr match {
-      case UnOp("-", UnOp("-", e)) => simplifyAll(e)
-      case BinOp("+", e, Number(0)) => simplifyAll(e)
-      case BinOp("*", e, Number(1)) => simplifyAll(e)
-      case UnOp("abs", e @ UnOp("abs", _)) => e
-      case UnOp(op, e) => UnOp(op, simplifyAll(e))
-      case BinOp(op, l, r) => BinOp(op, simplifyAll(l), simplifyAll(r))
-      case _ => expr
-    }
-  }
+  val e2 = BinOp(
+    "+",
+    BinOp("/", Var("x"), Number(2)),
+    BinOp("/", Number(1.5), Var("x"))
+  )
 
-  def checkOpKind(expr: Expr): Unit = {
-    expr match {
-      case BinOp(_, _, _) => println("It's a binary operation")
-      case UnOp(_, _) => println("It's a unary operation")
-      case _ => println("Operation not recognized")
-    }
-  }
+  val e3 = BinOp("/", e1, e2)
 
-  def describe(x: Any): String = {
-    x match {
-      case 5 => "five"
-      case true => "true"
-      case "hello" => "hi!"
-      case Nil => "empty list"
-      case _ => "something else"
-    }
-  }
+  def show(expr: Expr) = s"${println(f.format(expr))}\n\n"
 
-  def isPi: String = {
-    val pi = math.Pi
-    math.E match {
-      case `pi` => s"strange math? Pi = $pi"
-      case _ => "OK"
-    }
-  }
-
-  def isList(x: Any): String = {
-    x match {
-      case List(_*) => "Found a List!"
-      case _ => "Not yet!"
-    }
-  }
-
-  def generalSize(x: Any): Int = {
-    x match {
-      case s: String => s.length
-      case m: Map[_, _] /*or Map[k,v]*/ => m.size
-      case l: List[Any] => l.size
-      case a: Array[Any] => a.length
-      case _ => -1
-    }
-  }
-
-  def checkArgumentType(): Unit = {
-    val intMap = Map[Int, Int](1 -> 1)
-    val stringMap = Map[String, String]("abc" -> "abc")
-    val intArray = Array(1, 2, 3, 4)
-    val stringArray = Array("a", "b", "c", "d")
-
-    // Argument type is not checked in pattern matching
-    println(isIntIntMap(intMap))
-    println(isIntIntMap(stringMap))
-
-    // The only exception is for Array since arrays store their element type within them
-    println(isIntArray(intArray))
-    println(isIntArray(stringArray))
-
-    def isIntIntMap(x: Any): Boolean = {
-      x match {
-        case m: Map[Int, Int] => true
-        case _ => false
-      }
-    }
-
-    def isIntArray(x: Any): Boolean = {
-      x match {
-        case a: Array[Int] => true
-        case _ => false
-      }
-    }
-  }
-
-  def simplifyAdd(expr: Expr): Expr = {
-    expr match {
-      case BinOp("+", x, y) if x == y => BinOp("*", x, Number(2))
-      case _ => expr
-    }
-  }
-
-  // Gives us a compiler warning that not every possible match is present. It happens because the abstract class Expr is sealed. (sealed abstract class Expr)
-  def describe2(expr: Expr): String = {
-    (expr: @unchecked) match {
-      case Number(_) => "It's a number"
-      case Var(_) => "It's a variable"
-    }
-  }
-
-  def capitals(): Unit ={
-    val capitals: Map[String,String] = Map("France" -> "Paris", "Japan" -> "Tokyo")
-
-    println(show(capitals get "Japan"))
-    println(show(capitals get "North Pole"))
-
-    def show(key: Option[String]): String = {
-      key match {
-        case Some(s) => s
-        case None => "?"
-      }
-    }
-  }
-
-  def variablePatter(): Unit = {
-    val myTuple = (123, "abc")
-    val (myNumber, myString) = myTuple
-    println(myTuple)
-    println(myNumber)
-    println(myString)
-
-    // Usable with Case Classes
-    val myExpr = UnOp("abs", Var("x"))
-    val UnOp(op, value) = myExpr
-    println(myExpr)
-    println(op)
-    println(value)
-  }
-
-  def caseSequences(): Unit = {
-    val withDefault: Option[Int] => Int = {
-      case Some(i) => i
-      case None => 0
-    }
-
-    println(withDefault(Some(10)))
-    println(withDefault(None))
-
-    val second: PartialFunction[List[Int], Int] = {
-      case x :: y :: _ => y
-    }
-
-    val list = List()
-    if (second.isDefinedAt(list))
-      println(second(list))
-  }
-
-  def forPattern(): Unit = {
-    val capitals: Map[String,String] = Map("France" -> "Paris", "Japan" -> "Tokyo")
-    for ((country, city) <- capitals)
-      println(s"The capital of $country is $city")
-
-    val fruits = List(Some("Orange"), None, Some("Apple"))
-    for (fruit <- fruits) {
-      println(fruit)
-    }
-  }
+  for (e <- Array(e1, e2, e3))
+    show(e)
 }
---
+++ src/main/scala/com/github/fescalhao/chapter15/classes/ExprFormatter.scala
@@ -0,0 +1,65 @@
+// Case Classes and Pattern Matching
+
+package com.github.fescalhao.chapter15.classes
+
+import com.github.fescalhao.chapter10.abs.Element
+import com.github.fescalhao.chapter10.impl.ElementFactory
+
+class ExprFormatter {
+  private val opGroups = Array(
+    Set("|", "||"),
+    Set("&", "&&"),
+    Set("^"),
+    Set("==", "!="),
+    Set("<", "<=", ">=", ">"),
+    Set("+", "-"),
+    Set("*", "/")
+  )
+
+  private val precedence = {
+    val assocs = {
+      for {
+        i <- opGroups.indices
+        op <- opGroups(i)
+      } yield op -> i
+    }
+    assocs.toMap
+  }
+
+  private val unaryPrecedence = opGroups.length
+  private val fractionalPrecedence = -1
+
+  private def format(e: Expr, enclPrec: Int): Element = {
+    e match {
+      case Var(name) => ElementFactory.elem(name)
+
+      case Number(num) =>
+        def stripDot(s:String) =
+          if (s endsWith ".0")
+            s.substring(0, s.length - 2)
+          else
+            s
+        ElementFactory.elem(stripDot(num.toString))
+
+      case UnOp(operator, arg) => ElementFactory.elem(operator) beside format(arg, unaryPrecedence)
+
+      case BinOp("/", left, right) =>
+        val top = format(left, fractionalPrecedence)
+        val bot = format(right, fractionalPrecedence)
+        val line = ElementFactory.elem('-', 1, top.width max bot.width)
+        val frac = top above line above bot
+        if (enclPrec != fractionalPrecedence) frac
+        else ElementFactory.elem(" ") beside frac beside ElementFactory.elem(" ")
+
+      case BinOp(operator, left, right) =>
+        val opPrec = precedence(operator)
+        val l = format(left, opPrec)
+        val r = format(right, opPrec + 1)
+        val oper = l beside ElementFactory.elem(s" $operator ") beside r
+        if (enclPrec <= opPrec) oper
+        else ElementFactory.elem("(") beside oper beside ElementFactory.elem(")")
+    }
+  }
+
+  def format(e: Expr): Element = format(e, 0)
+}
