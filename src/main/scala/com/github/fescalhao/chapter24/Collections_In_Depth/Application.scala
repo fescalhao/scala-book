612ad61fc3695ffc5225cc5962c757865a9268fd
Felipe Alberto Silva Escalhao <fescalhao@uolinc.com>
2020-12-28 15:20:28 -0300

    Collections in depth

---
+++ src/main/scala/com/github/fescalhao/chapter24/Collections_In_Depth/Application.scala
@@ -0,0 +1,8 @@
+package com.github.fescalhao.chapter24.Collections_In_Depth
+
+import com.github.fescalhao.chapter24.Collections_In_Depth.classes.Example
+
+object Application extends App {
+  val example = new Example
+  example.executeExamples()
+}
